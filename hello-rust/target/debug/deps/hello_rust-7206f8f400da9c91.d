10b1b8a026db09781115707850545dc255038190
James Neilson <james@quandl.com>
2019-04-07 19:12:12 -0400

    Start new hello-rust

---
+++ hello-rust/target/debug/deps/hello_rust-7206f8f400da9c91.d
@@ -0,0 +1,5 @@
+/Users/jamesneilson/Documents/personal_projects/rust_tutorials/hello-rust/target/debug/deps/hello_rust-7206f8f400da9c91: src/main.rs
+
+/Users/jamesneilson/Documents/personal_projects/rust_tutorials/hello-rust/target/debug/deps/hello_rust-7206f8f400da9c91.d: src/main.rs
+
+src/main.rs:
