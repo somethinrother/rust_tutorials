23d67875eb44bd328cc4dda18f2a0e48a9dc27b9
James Neilson <james@quandl.com>
2019-04-07 23:47:29 -0400

    Copy in struct info

---
+++ custom-types/target/debug/custom-types.d
@@ -0,0 +1,1 @@
+/Users/jamesneilson/Documents/personal_projects/rust_tutorials/custom-types/target/debug/custom-types.dSYM: /Users/jamesneilson/Documents/personal_projects/rust_tutorials/custom-types/src/main.rs
---
+++ custom-types/target/debug/deps/custom_types-9952a2be264c1273.d
@@ -0,0 +1,5 @@
+/Users/jamesneilson/Documents/personal_projects/rust_tutorials/custom-types/target/debug/deps/custom_types-9952a2be264c1273: src/main.rs
+
+/Users/jamesneilson/Documents/personal_projects/rust_tutorials/custom-types/target/debug/deps/custom_types-9952a2be264c1273.d: src/main.rs
+
+src/main.rs:
