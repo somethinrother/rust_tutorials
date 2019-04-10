201d84f9e0f828b3020c3cbb29b714fdb7bd3e20
James Neilson <james@quandl.com>
2019-04-10 19:40:19 -0400

    Going through inference

--- types/src/main.rs
+++ types/src/main.rs
@@ -71,4 +71,24 @@ fn main() {
     println!("size of `z` in bytes: {}", std::mem::size_of_val(&z));
     println!("size of `i` in bytes: {}", std::mem::size_of_val(&i));
     println!("size of `f` in bytes: {}", std::mem::size_of_val(&f));
+
+    // INFERENCE
+    println!("");
+    println!("********** INFERENCE **********");
+    println!("");
+
+    // Because of the annotation, the compiler knows that `elem` has type u8.
+    let elem = 5u8;
+
+    // Create an empty vector (a growable array).
+    let mut vec = Vec::new();
+    // At this point the compiler doesn't know the exact type of `vec`, it
+    // just knows that it's a vector of something (`Vec<_>`).
+
+    // Insert `elem` in the vector.
+    vec.push(elem);
+    // Aha! Now the compiler knows that `vec` is a vector of `u8`s (`Vec<u8>`)
+    // TODO ^ Try commenting out the `vec.push(elem)` line
+
+    println!("{:?}", vec);
 }
