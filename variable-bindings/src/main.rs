76f974dd7bebd7d6e8f96fc991ce9a382e0dfd98
James Neilson <james@quandl.com>
2019-04-10 11:27:20 -0400

    Lesson on declaring first method

--- variable-bindings/src/main.rs
+++ variable-bindings/src/main.rs
@@ -74,4 +74,31 @@ fn main() {
     let long_lived_binding = 'a';
 
     println!("outer long: {}", long_lived_binding);
+
+    // DECLARE FIRST
+    println!("");
+    println!("********** DECLARE FIRST **********");
+    println!("");
+
+    // Declare a variable binding
+    let a_binding;
+
+    {
+        let x = 2;
+
+        // Initialize the binding
+        a_binding = x * x;
+    }
+
+    println!("a binding: {}", a_binding);
+
+    let another_binding;
+
+    // Error! Use of uninitialized binding
+    // println!("another binding: {}", another_binding);
+    // FIXME ^ Comment out this line
+
+    another_binding = 1;
+
+    println!("another binding: {}", another_binding);
 }
