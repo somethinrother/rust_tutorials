791951f9c5e2ee3baecba14f59060a9621b3c2fd
James Neilson <james@quandl.com>
2019-04-10 19:43:34 -0400

    Finish chapter on aliasing

--- types/src/main.rs
+++ types/src/main.rs
@@ -1,6 +1,15 @@
 // Suppress all warnings from casts which overflow.
 #![allow(overflowing_literals)]
 
+// `NanoSecond` is a new name for `u64`.
+type NanoSecond = u64;
+type Inch = u64;
+
+// Use an attribute to silence warning.
+#[allow(non_camel_case_types)]
+type u64_t = u64;
+// TODO ^ Try removing the attribute
+
 fn main() {
     // CASTING
     println!("");
@@ -91,4 +100,20 @@ fn main() {
     // TODO ^ Try commenting out the `vec.push(elem)` line
 
     println!("{:?}", vec);
+
+    // ALIASING
+    println!("");
+    println!("********** ALIASING **********");
+    println!("");
+
+    // `NanoSecond` = `Inch` = `u64_t` = `u64`.
+    let nanoseconds: NanoSecond = 5 as u64_t;
+    let inches: Inch = 2 as u64_t;
+
+    // Note that type aliases *don't* provide any extra type safety, because
+    // aliases are *not* new types
+    println!("{} nanoseconds + {} inches = {} unit?",
+             nanoseconds,
+             inches,
+             nanoseconds + inches);
 }
