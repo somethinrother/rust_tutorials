e078a979c111f7c05c18c2b6ffe797d951031e86
James Neilson <james@quandl.com>
2019-04-09 21:46:37 -0400

    Constants!

---
+++ constants/target/debug/constants.d
@@ -0,0 +1,1 @@
+/Users/jamesneilson/Documents/personal_projects/rust_tutorials/constants/target/debug/constants.dSYM: /Users/jamesneilson/Documents/personal_projects/rust_tutorials/constants/src/main.rs
---
+++ constants/target/debug/deps/constants-ca4df2f7458fc080.d
@@ -0,0 +1,5 @@
+/Users/jamesneilson/Documents/personal_projects/rust_tutorials/constants/target/debug/deps/constants-ca4df2f7458fc080: src/main.rs
+
+/Users/jamesneilson/Documents/personal_projects/rust_tutorials/constants/target/debug/deps/constants-ca4df2f7458fc080.d: src/main.rs
+
+src/main.rs:
